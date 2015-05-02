4d9f90a112f221540602bfd9dc349db3436f48c6
doxas <eizanco@gmail.com>
2015-05-02 14:04:05 +0900

    sample 004 add and 003 to copy sample/

--- sample/003/script/script.js
+++ sample/003/script/script.js
@@ -20,16 +20,20 @@ window.onload = function(){
 
 	// mesh data
 	var position = [
-		-0.5,  0.5,  0.0,
-		 0.5,  0.5,  0.0,
+		 0.0,  0.5,  0.0,
+		 0.5, -0.5,  0.0,
 		-0.5, -0.5,  0.0,
-		 0.5, -0.5,  0.0
+		 0.0, -0.5,  0.0,
+		 0.5,  0.5,  0.0,
+		-0.5,  0.5,  0.0
 	];
 	var color = [
 		1.0, 0.0, 0.0, 1.0,
 		0.0, 1.0, 0.0, 1.0,
 		0.0, 0.0, 1.0, 1.0,
-		1.0, 1.0, 1.0, 1.0
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0
 	];
 
 	// vertex buffer object
@@ -37,13 +41,6 @@ window.onload = function(){
 		gl3.create_vbo(position),
 		gl3.create_vbo(color)
 	];
-
-	// index buffer object
-	var index = [
-		0, 1, 2,
-		2, 1, 3
-	];
-	var IBO = gl3.create_ibo(index);
 
 	// rendering
 	render();
@@ -53,9 +50,9 @@ window.onload = function(){
 		gl3.scene_view(null, 0, 0, gl3.canvas.width, gl3.canvas.height);
 
 		prg.set_program();
-		prg.set_attribute(VBO, IBO);
+		prg.set_attribute(VBO);
 
-		gl3.draw_elements(gl3.gl.TRIANGLES, index.length);
+		gl3.draw_arrays(gl3.gl.TRIANGLES, 6);
 	}
 };
 
