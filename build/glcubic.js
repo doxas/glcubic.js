4e1c156c3d3fc91c5e025fc49d5cfa6fd552a5da
doxas <eizanco@gmail.com>
2016-07-30 16:28:36 +0900

    rebuild

--- build/glcubic.js
+++ build/glcubic.js
@@ -3,7 +3,7 @@
 var gl3 = gl3 || {};
 
 // const
-gl3.VERSION = '0.0.3';
+gl3.VERSION = '0.0.4';
 gl3.PI2  = 6.28318530717958647692528676655900576;
 gl3.PI   = 3.14159265358979323846264338327950288;
 gl3.PIH  = 1.57079632679489661923132169163975144;
@@ -153,11 +153,11 @@ gl3.program = {
         var src = {
             vs: {
                 targetUrl: vsUrl,
-                source: null,
+                source: null
             },
             fs: {
                 targetUrl: fsUrl,
-                source: null,
+                source: null
             }
         };
         xhr(src.vs);
@@ -165,6 +165,8 @@ gl3.program = {
         function xhr(target){
             var xml = new XMLHttpRequest();
             xml.open('GET', target.targetUrl, true);
+            xml.setRequestHeader('Pragma', 'no-cache');
+            xml.setRequestHeader('Cache-Control', 'no-cache');
             xml.onload = function(){
                 console.log('%c◆%c shader source loaded: %c' + target.targetUrl, 'color: crimson', '', 'color: goldenrod');
                 target.source = xml.responseText;
@@ -359,8 +361,8 @@ gl3.create_texture = function(source, number, callback){
     var img = new Image();
     var self = this;
     var gl = this.gl;
-    this.textures[number] = {texture: null, type: null, loaded: false};
     img.onload = function(){
+        self.textures[number] = {texture: null, type: null, loaded: false};
         var tex = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, tex);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
@@ -1195,20 +1197,23 @@ gl3.mesh = {
 };
 
 
+
 // step 1: var a = new AudioCtr(bgmGainValue, soundGainValue) <- float(0 to 1)
 // step 2: a.load(url, index, loop, background) <- string, int, boolean, boolean
 // step 3: a.src[index].loaded then a.src[index].play()
 
 // audio controler
-function AudioCtr(bgmGainValue, soundGainValue){
+gl3.audioCtr = function(){};
+
+gl3.audioCtr.prototype.init = function(bgmGainValue, soundGainValue){
     if(
-        typeof webkitAudioContext != 'undefined' ||
-        typeof AudioContext != 'undefined'
+        typeof AudioContext != 'undefined' ||
+        typeof webkitAudioContext != 'undefined'
     ){
-        if(typeof webkitAudioContext != 'undefined'){
-            this.ctx = new webkitAudioContext();
-        }else{
+        if(typeof AudioContext != 'undefined'){
             this.ctx = new AudioContext();
+        }else{
+            this.ctx = new webkitAudioContext();
         }
         this.comp = this.ctx.createDynamicsCompressor();
         this.comp.connect(this.ctx.destination);
@@ -1218,31 +1223,34 @@ function AudioCtr(bgmGainValue, soundGainValue){
         this.soundGain = this.ctx.createGain();
         this.soundGain.connect(this.comp);
         this.soundGain.gain.value = soundGainValue;
-        this.src = new Array();
+        this.src = [];
     }else{
         return null;
     }
-}
+};
 
-AudioCtr.prototype.load = function(url, index, loop, background){
+gl3.audioCtr.prototype.load = function(url, index, loop, background, callback){
     var ctx = this.ctx;
     var gain = background ? this.bgmGain : this.soundGain;
     var src = this.src;
     src[index] = null;
     var xml = new XMLHttpRequest();
     xml.open('GET', url, true);
+    xml.setRequestHeader('Pragma', 'no-cache');
+    xml.setRequestHeader('Cache-Control', 'no-cache');
     xml.responseType = 'arraybuffer';
     xml.onload = function(){
         ctx.decodeAudioData(xml.response, function(buf){
-            src[index] = new AudioSrc(ctx, gain, buf, loop);
+            src[index] = new gl3.audioSrc(ctx, gain, buf, loop, background);
             src[index].loaded = true;
-            console.log('audio loaded : index ' + index + '; ')
+            console.log('%c◆%c audio number: %c' + index + '%c, audio loaded: %c' + url, 'color: crimson', '', 'color: blue', '', 'color: goldenrod');
+            callback();
         }, function(e){console.log(e);});
     };
     xml.send();
 };
 
-AudioCtr.prototype.loadComplete = function(){
+gl3.audioCtr.prototype.loadComplete = function(){
     var i, f;
     f = true;
     for(i = 0; i < this.src.length; i++){
@@ -1252,17 +1260,27 @@ AudioCtr.prototype.loadComplete = function(){
 };
 
 // audio source
-function AudioSrc(ctx, gain, audioBuffer, loop){
+gl3.audioSrc = function(ctx, gain, audioBuffer, loop, background){
     this.ctx = ctx;
     this.gain = gain;
     this.audioBuffer = audioBuffer;
-    this.bufferSource = new Array();
+    this.bufferSource = [];
+    this.activeBufferSource = 0;
     this.loop = loop;
     this.loaded = false;
-}
+    this.fftLoop = 16;
+    this.update = false;
+    this.background = background;
+    this.node = this.ctx.createScriptProcessor(2048, 1, 1);
+    this.analyser = this.ctx.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.8;
+    this.analyser.fftSize = this.fftLoop * 2;
+    this.onData = new Uint8Array(this.analyser.frequencyBinCount);
+};
 
-AudioSrc.prototype.play = function(){
+gl3.audioSrc.prototype.play = function(){
     var i, j, k;
+    var self = this;
     i = this.bufferSource.length;
     k = -1;
     if(i > 0){
@@ -1282,6 +1300,7 @@ AudioSrc.prototype.play = function(){
         this.bufferSource[0] = this.ctx.createBufferSource();
         k = 0;
     }
+    this.activeBufferSource = k;
     this.bufferSource[k].buffer = this.audioBuffer;
     this.bufferSource[k].loop = this.loop;
     this.bufferSource[k].playbackRate.value = 1.0;
@@ -1291,13 +1310,28 @@ AudioSrc.prototype.play = function(){
             this.playnow = false;
         };
     }
+    if(this.background){
+        this.bufferSource[k].connect(this.analyser);
+        this.analyser.connect(this.node);
+        this.node.connect(this.ctx.destination);
+        this.node.onaudioprocess = function(eve){onprocessEvent(eve);};
+    }
     this.bufferSource[k].connect(this.gain);
     this.bufferSource[k].start(0);
-    this.bufferSource[k].playnow = true; // custom property
+    this.bufferSource[k].playnow = true;
+
+    function onprocessEvent(eve){
+        if(self.update){
+            self.update = false;
+            self.analyser.getByteFrequencyData(self.onData);
+        }
+    }
 };
 
-AudioSrc.prototype.end = function(index){
-    this.bufferSource[index].stop(0);
+gl3.audioSrc.prototype.stop = function(){
+    this.bufferSource[this.activeBufferSource].stop(0);
     this.playnow = false;
 };
+
+gl3.audio = new gl3.audioCtr();
 
